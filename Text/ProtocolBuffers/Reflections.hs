24bae4e577ad525bde98b6741b509ed21ebb4c13
Chris Kuklewicz <haskell@list.mightyreason.com>
2008-07-21 17:10:46 +0000

    Make cEncode and cDecode match google's code (not too pretty)

--- Text/ProtocolBuffers/Reflections.hs
+++ Text/ProtocolBuffers/Reflections.hs
@@ -18,13 +18,15 @@ import qualified Data.ByteString.UTF8 as U
 import qualified Data.ByteString as BS (ByteString,null,pack,unpack)
 import qualified Data.ByteString.Char8 as BSC(pack,unpack)
 import Numeric
-import Data.Char(ord,chr)
+import Data.Char(ord,chr,isHexDigit,isOctDigit,toLower)
 import Data.List(unfoldr)
 import Data.Word(Word8)
 import Data.Map(Map)
 import Text.ProtocolBuffers.Basic
 import Data.Generics(Data)
 import Data.Typeable(Typeable)
+import Test.QuickCheck(quickCheck)
+import Codec.Binary.UTF8.String(encode)
 
 data ProtoName = ProtoName { haskellPrefix :: String  -- Haskell specific prefix to module hierarchy (e.g. Text)
                            , parentModule :: String   -- Proto specified namespace (like java)
@@ -93,26 +95,6 @@ parseDefString bs = Just (HsDef'ByteString bs)
 parseDefBytes :: ByteString -> Maybe HsDefault
 parseDefBytes bs = Just . HsDef'ByteString 
                    . BS.pack . cDecode . BSC.unpack $ bs
-
-cEncode :: [Word8] -> [Char]
-cEncode = concatMap one where
-  one 0 = ['\\','0']
-  one 92 = ['\\','\\']
-  one x | x > 127 = '\\':(showOct x "")
-  one x = [toEnum .  fromEnum $  x]
-
-cDecode :: [Char] -> [Word8]
-cDecode = unfoldr one where
-  one ('\\':'\\':xs) = Just (toEnum . fromEnum $ '\\',xs)
-  one ('\\':'0':xs) = Just (0,xs)
-  one ('\\':ys@(n:_:_:xs)) | '2'==n || '3' == n =
-   case mayRead readOct (take 3 ys) of
-     Just w -> Just (w,xs)
-     Nothing -> Nothing
-  one ('\\':_) = error "Text.ProtocolBuffers.Reflections.cDecode cannot understand your backslash escape value"
-  one (x:xs) = Just (toEnum . fromEnum $ x,xs)
-  one [] = Nothing
-
 parseDefInteger :: ByteString -> Maybe HsDefault
 parseDefInteger bs = fmap HsDef'Integer . mayRead checkSign . U.toString $ bs
     where checkSign = readSigned' checkBase
@@ -120,11 +102,101 @@ parseDefInteger bs = fmap HsDef'Integer . mayRead checkSign . U.toString $ bs
           checkBase ('0':xs) = readOct xs
           checkBase xs = readDec xs
 
-readSigned' f ('-':xs) = map (\(v,s) -> (-v,s)) . f $ xs
-readSigned' f ('+':xs) = f xs
-readSigned' f xs = f xs
-
 parseDefBool :: ByteString -> Maybe HsDefault
 parseDefBool bs | bs == BSC.pack "true" = Just (HsDef'Bool True)
                 | bs == BSC.pack "false" = Just (HsDef'Bool False)
                 | otherwise = Nothing
+
+-- The Numeric.readSigned does not handle '+' for some odd reason
+readSigned' f ('-':xs) = map (\(v,s) -> (-v,s)) . f $ xs
+readSigned' f ('+':xs) = f xs
+readSigned' f xs = f xs
+
+-- see google's stubs/strutil.cc lines 398-449/1121 and C99 specification
+-- This mainly targets three digit octal codes
+cEncode :: [Word8] -> [Char]
+cEncode = concatMap one where
+  one :: Word8 -> [Char]
+  one x | (32 < x) && (x < 127) = [toEnum .  fromEnum $  x]  -- main case of unescaped value
+  one 9 = sl  't'
+  one 10 = sl 'n'
+  one 13 = sl 'r'
+  one 34 = sl '"'
+  one 39 = sl '\''
+  one 92 = sl '\\'
+  one 0 = '\\':"000"
+  one x | x < 8 = '\\':'0':'0':(showOct x "")
+        | x < 32 = '\\':'0':(showOct x "")
+        | otherwise = '\\':(showOct x "")
+  sl c = ['\\',c]
+
+-- Takes backslash encoded junk an retrieves the underlying bytes
+-- The \x is kosher, but \X is non-standard.  I recognize \X because google does
+-- Convert backslash-escaped \u and \U crap into UTF-8 (this is why we have to use concat!)
+-- It only works on characters and values in the range 0..255 (the Char8's returned by ByteString.Char.unpack)
+-- It ONLY consumes up to 3 octal digits when reading an escape sequence, the rest are left alone.
+cDecode :: [Char] -> [Word8]
+cDecode = concat . unfoldr one where
+  one :: [Char] -> Maybe ([Word8],[Char])
+  one (x:xs) | x /= '\\' = Just (checkChar8 x,xs)  -- main case of unescaped value
+  one [] = Nothing
+  one ('\\':[]) = error "Text.ProtocolBuffers.Reflections.cDecode cannot understand ending with a backslash"
+  one ('\\':ys) | 1 <= len =
+      case mayRead readOct oct of
+        Just w -> Just (checkByte w,rest)
+        Nothing -> error "Text.ProtocolBuffers.Reflections.cDecode failed to decode octal sequence"
+    where oct = takeWhile isOctDigit (take 3 ys)
+          len = length oct
+          rest = drop len ys
+  one ('\\':x:ys) | 'x' == toLower x && ok =
+      case mayRead readHex hex of
+        Just w -> Just (checkByte w,rest)
+        Nothing -> error "Text.ProtocolBuffers.Reflections.cDecode failed to decode hex sequence"
+    where ok = 1 <= length hex
+          (hex,rest) = span isHexDigit ys
+  one ('\\':'u':ys) | ok =
+      case mayRead readHex hex of
+        Just w -> Just (checkUnicode w,rest)
+        Nothing -> error "Text.ProtocolBuffers.Reflections.cDecode failed to decode 4 char unicode sequence"
+    where ok = all isHexDigit hex && 4 == length hex
+          (hex,rest) = splitAt 4 ys
+  one ('\\':'U':ys) | ok =
+      case mayRead readHex hex of
+        Just w -> Just (checkUnicode w,rest)
+        Nothing -> error "Text.ProtocolBuffers.Reflections.cDecode failed to decode 8 char unicode sequence"
+    where ok = all isHexDigit hex && 8 == length hex
+          (hex,rest) = splitAt 8 ys
+  one ('\\':(x:xs)) = Just ([decode x],xs)
+  decode :: Char -> Word8
+  decode 'a' = 7
+  decode 'b' = 8
+  decode 't' = 9
+  decode 'n' = 10
+  decode 'v' = 11
+  decode 'f' = 12
+  decode 'r' = 13
+  decode '\"' = 34
+  decode '\'' = 39
+  decode '?' = 63    -- C99 rule : "\?" is '?'
+  decode '\\' = 92
+  decode x | toLower x == 'x' = error "Text.ProtocolBuffers.Reflections.cDecode cannot understand your 'xX' hexadecimal escaped value"
+  decode x | toLower x == 'u' = error "Text.ProtocolBuffers.Reflections.cDecode cannot understand your 'uU' unicode UTF-8 hexadecimal escaped value"
+  decode _ = error "Text.ProtocolBuffers.Reflections.cDecode cannot understand your backslash-escaped value"
+  checkChar8 :: Char -> [Word8]
+  checkChar8 c | (0 <= i) && (i <= 255) = [toEnum i]
+               | otherwise = error "Text.ProtocolBuffers.Reflections.cDecode found Char out of range 0..255"
+    where i = fromEnum c
+  checkByte :: Integer -> [Word8]
+  checkByte i | (0 <= i) && (i <= 255) = [fromInteger i]
+              | otherwise = error "Text.ProtocolBuffers.Reflections.cDecode found Oct/Hex Int out of range 0..255"
+  checkUnicode :: Integer -> [Word8]
+  checkUnicode i | (0 <= i) && (i <= 127) = [fromInteger i]
+                 | i <= maxChar = encode [ toEnum . fromInteger $ i ]
+                 | otherwise = error "Text.ProtocolBuffers.Reflections.cDecode found Unicode Char out of range 0..0x10FFFF"
+    where maxChar = toInteger (fromEnum (maxBound ::Char)) -- 0x10FFFF
+
+
+testEncodeDecode = let q :: [Int] -> Bool
+                       q =  (\y -> let x = map (\z->abs z `mod` 255) y
+                                   in  x == (map fromEnum . cDecode . cEncode . map toEnum$ x))
+                   in quickCheck q
