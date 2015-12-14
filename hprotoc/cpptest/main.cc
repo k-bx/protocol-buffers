b204985b0887e3c03e4f406cc484a535aa568b51
Ian-Woo Kim <ianwookim@gmail.com>
2015-12-14 09:57:34 +0000

    parsing example.

--- hprotoc/cpptest/main.cc
+++ hprotoc/cpptest/main.cc
@@ -1,14 +1,18 @@
+#include <fcntl.h>
 #include <iostream>
+#include <fstream>
+#include <streambuf>
 #include <string>
 
 #include <google/protobuf/text_format.h>
+#include <google/protobuf/io/zero_copy_stream_impl.h>
 #include "sample.pb.h"
 
 using namespace std;
 
-int main(int argc, char* argv[])
+int main1(int argc, char* argv[])
 {
-  GOOGLE_PROTOBUF_VERIFY_VERSION;
+  //GOOGLE_PROTOBUF_VERIFY_VERSION;
 
   sample_message a;
   sample_message_user* u1;
@@ -22,6 +26,10 @@ int main(int argc, char* argv[])
   u1 = a.add_users( );
   u1->set_id( 30 );
   u1->set_name( "hello" );
+
+  u1 = a.add_users();
+  u1->set_id( 45 );
+  u1->set_name( "world");
   
   // a.SerializeToString(&data);
   google::protobuf::TextFormat::PrintToString( a , &data );
@@ -31,4 +39,31 @@ int main(int argc, char* argv[])
   
 
   
+}
+
+
+int main(int argc, char* argv[])
+{
+  GOOGLE_PROTOBUF_VERIFY_VERSION;
+
+  if( argc != 2 ) {
+    cerr << "Usage: " << argv[0] << " File" << endl;
+  }
+  
+  sample_message *a = new sample_message();
+  ifstream input;
+  input.open(argv[1]);
+
+  int fileDescriptor = open(argv[1],O_RDONLY);
+  google::protobuf::io::FileInputStream fileInput( fileDescriptor );
+  // string data(istreambuf_iterator<char>(input), istreambuf_iterator< char>() );
+  
+  google::protobuf::TextFormat::Parse(&fileInput, a ) ;
+
+  string text; 
+  google::protobuf::TextFormat::PrintToString( *a , &text );
+
+  cout << text << endl;
+  
+    
 }
