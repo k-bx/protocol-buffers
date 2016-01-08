fdcd2b03fb06fa378762ca0c2c387f55643df67e
Ian-Woo Kim <ianwookim@gmail.com>
2016-01-08 21:33:18 +0000

    gcc -> g++ in build.sh

--- hprotoc/cpptest/build.sh
+++ hprotoc/cpptest/build.sh
@@ -1,4 +1,4 @@
 protoc --proto_path=.. --cpp_out=. ../sample.proto
-gcc -c main.cc `pkg-config --cflags protobuf`
-gcc -c sample.pb.cc `pkg-config --cflags protobuf`
-gcc -o main main.o sample.pb.o `pkg-config --libs protobuf`
+g++ -c main.cc `pkg-config --cflags protobuf`
+g++ -c sample.pb.cc `pkg-config --cflags protobuf`
+g++ -o main main.o sample.pb.o `pkg-config --libs protobuf`
