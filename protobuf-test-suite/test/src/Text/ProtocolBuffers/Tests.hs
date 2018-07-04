3ef68b8e8c242d069031e6b813a097cafefec2a4
Steve King <stevejohnking@gmail.com>
2018-07-03 21:49:48 -0700

    Add QuickChecks for oneof

--- protobuf-test-suite/test/src/Text/ProtocolBuffers/Tests.hs
+++ protobuf-test-suite/test/src/Text/ProtocolBuffers/Tests.hs
@@ -6,9 +6,11 @@ module Text.ProtocolBuffers.Tests
 import Test.Tasty (defaultMain, testGroup)
 
 import Text.ProtocolBuffers.Tests.AddressBook (addressBookTests, addressBookQuickChecks)
+import Text.ProtocolBuffers.Tests.School (schoolQuickChecks)
 
 tests :: IO ()
 tests = defaultMain $ testGroup "Protocol Buffers tests" $
   [ addressBookTests
   , addressBookQuickChecks
+  , schoolQuickChecks
   ]
